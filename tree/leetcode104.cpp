f0d6cbb186fc569d6c2ef1a9c3fcf618dddf7a0e
lele12 <2842408592@qq.com>
2020-12-11 01:20:13 +0800

    max depth

--- tree/leetcode104.cpp
+++ tree/leetcode104.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -20,6 +22,70 @@ int maxDepth(TreeNode* root){
 }
 
 
+void makeTree(TreeNode* &root, vector<int>& nums, int i){
+    if (i >= nums.size()){
+        return;
+    }
+    root = new TreeNode(nums[i]);
+    makeTree(root->left, nums, 2*i+1);
+    makeTree(root->right, nums, 2*i+2);
+    return;
+}
+
+void pre_order(TreeNode* root){
+    if (root == nullptr)
+        return;
+    cout << root->val << "  ";
+    pre_order(root->left);
+    pre_order(root->right);
+}
+
+void mid_order(TreeNode* root){
+    if (root == nullptr)
+        return;
+    mid_order(root->left);
+    cout << root->val << "  ";
+    mid_order(root->right);
+}
+
+void post_order(TreeNode* root){
+    if (root == nullptr)
+        return;
+    post_order(root->left);
+    post_order(root->right);
+    cout << root->val << "  ";
+
+}
+
+void add(queue<TreeNode*>& que, TreeNode* root){
+        if (root != nullptr)
+            que.push(root);
+        return;
+}
+
+void layer_order(TreeNode* root){
+    if (root == nullptr)
+        return;
+    queue<TreeNode *> que;
+    que.push(root);
+    while(!que.empty()){
+        TreeNode *leaf = que.front();
+        cout << leaf->val << "  ";
+        add(que, leaf->left);
+        add(que, leaf->right);
+        que.pop();    
+    }
+    return;
+}
+
+
+
 int main(){
+    vector<int> nums = {3, 9, 20, -1, -1, 15, 7};
+    TreeNode *root;
+    int i = 0;
+    makeTree(root, nums, i);
+    layer_order(root);
+    system("pause");
     return 0;
 }
