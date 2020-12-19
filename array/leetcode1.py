dd25e3bae538e2d92fcad96c9746f3b9f8fa1276
caozhihui <caozhihui@untouch-tech.com>
2020-12-19 15:51:58 +0800

    [INIT] init leetcode 1

---
+++ array/leetcode1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    @Header leetcode1
+    @Author fosaken
+    @Date   2020/12/19
+    @Abstract:
+        Address: https://leetcode-cn.com/problems/two-sum/submissions/
+"""
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l = len(nums)
+        for i in range(l):
+            for j in range(i + 1, l):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
