6065779d9b039e73b9f97e3a8ed187184dd3bf07
lele <18710448981@163.com>
2020-12-19 17:48:43 +0800

    add 572

--- tree/leetcode617.cpp
+++ tree/leetcode617.cpp
@@ -5,13 +5,17 @@
 using namespace std;
 
 TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2){
-    if (t1 == nullptr){
-        return t2;
+    if (t1 == nullptr and t2 == nullptr){
+        return nullptr;
     }
-    if (t2 == nullptr){
+    else if (t1 == nullptr and t2 != nullptr){
+        t1 = new TreeNode(t2->val);
+    }else if (t1 != nullptr and t2 == nullptr){
         return t1;
+    }else{
+        t1->val += t2->val;
     }
-    t1->val += t2->val;
+
     t1->left = mergeTrees(t1->left, t2->left);
     t1->right = mergeTrees(t1->right, t2->right);
     return t1;
