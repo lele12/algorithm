50764bd5b078e7b509e74d1f92fec0bc0ea96eca
lele12 <2842408592@qq.com>
2020-12-30 00:22:25 +0800

    add 111

--- tree/leetcode111.cpp
+++ tree/leetcode111.cpp
@@ -1,36 +1,53 @@
 #include <iostream>
+#include <queue>
 #include "tree.h"
 
 using namespace std;
 
-void traverse(TreeNode* root, int& min, int step){
-    if (root == nullptr){
-        if (step < min){
-            min = step;
-        }
-        return;
-    }
-    traverse(root->left, min, step + 1);
-    traverse(root->right, min, step + 1);
-    return;
-}
+// int minDepth(TreeNode* root){
+//     if (root == nullptr){
+//         return 0;
+//     }
+//     if (root->left == nullptr && root->right == nullptr){
+//         return 1;
+//     }
+//     int left = minDepth(root->left);
+//     int right = minDepth(root->right);
+//     if (left == 0) {
+//         return right + 1;
+//     }
+//     if (right == 0){
+//         return left + 1;
+//     }
+//     return min(left+1, right+1);
+// }
 
 int minDepth(TreeNode* root){
     if (root == nullptr){
         return 0;
     }
-    if (root->left == nullptr && root->right == nullptr){
-        return 1;
+    int step = 1;
+    queue<TreeNode *> q;
+    q.push(root);
+    while(!q.empty()){
+        int q_size = q.size();
+        for (int i = 0; i < q_size; i++){
+            
+            TreeNode *cur = q.front();
+            q.pop();
+            if (cur->left == nullptr && cur->right== nullptr){
+                return step;
+            }
+            if (cur->left != nullptr){
+                q.push(cur->left);
+            }
+            if (cur->right != nullptr){
+                q.push(cur->right);
+            }
+        }
+        step++;
     }
-    int left = minDepth(root->left);
-    int right = minDepth(root->right);
-    if (left == 0) {
-        return right + 1;
-    }
-    if (right == 0){
-        return left + 1;
-    }
-    return min(left+1, right+1);
+    return step;
 }
 
 int main(){
