88632ac9d7cb0aa9fe70fb8d9507e162c8cde460
lele12 <2842408592@qq.com>
2021-01-07 00:24:04 +0800

    add 300

--- dynamic-programming/leetcode494.cpp
+++ dynamic-programming/leetcode494.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <ext/hash_map>
 
 using namespace std;
+using  namespace   __gnu_cxx;
 
+// void findWays(vector<int> &nums, int &res, int S, int k, int pos){
+    
+//     if (k == S && pos == nums.size()){
+//         res += 1;
+//     }
+//     if (pos > nums.size()-1){
+//         return;
+//     }
+    
+//     k += nums[pos];
+//     findWays(nums, res, S, k, pos + 1);
+//     k -= nums[pos];
+
+//     k -= nums[pos];
+//     findWays(nums, res, S, k, pos + 1);
+//     k += nums[pos];
+// }
+hash_map<string, int> memo;
 void findWays(vector<int> &nums, int &res, int S, int k, int pos){
     
     if (k == S && pos == nums.size()){
@@ -11,18 +32,26 @@ void findWays(vector<int> &nums, int &res, int S, int k, int pos){
     if (pos > nums.size()-1){
         return;
     }
-    // cout << pos << "  " << k << endl;
-    k += nums[pos];
-    findWays(nums, res, S, k, pos + 1);
-    k -= nums[pos];
+    string key = to_string(pos) + "," + to_string(k);
+    if(memo.find(key) != memo.end()){
+        res += memo[key];
+        return;
+    }else{
+        k += nums[pos];
+        findWays(nums, res, S, k, pos + 1);
+        k -= nums[pos];
 
-    k -= nums[pos];
-    findWays(nums, res, S, k, pos + 1);
-    k += nums[pos];
+        k -= nums[pos];
+        findWays(nums, res, S, k, pos + 1);
+        k += nums[pos];
+        memo[key] = res;
+    }
+
 }
 
 int findTargetSumWays(vector<int>& nums, int S){
     int res = 0;
+
     findWays(nums, res, S, 0, 0);
     return res;
 }
